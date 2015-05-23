53d116ed8d8331fd59b38e27a782e288ccf61533
SuperShmoo <wermold@apl.washington.edu>
2015-05-23 12:16:45 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to cache the inverse of a matrix.
+## Step 1: Feed your matrix into makeCacheMatrix:
+##         If "mat" is your matrix then: matcache <- makeCacheMatrix(mat)
+## Step 2: mat_inverse <- cacheSolve(matcache)
+##         This will return the inverse of "mat"
 
-## Write a short comment describing this function
 
+##This function generates a list of functions to cache information about your matrix. 
+##Both the matrix itelf and its inverse are cached
 makeCacheMatrix <- function(x = matrix()) {
-
+        ix <- NULL
+        set <- function(y) {
+                x <<- y
+                ix <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) ix <<- inverse
+        getinverse <- function() ix
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## This function searches to see if the inverse of a matrix has been cached. If it has, the
+##cached inverse is retrieved. If it has not been cached yet, the inverse is calculated and then
+##cached, and the inverse is then returned. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ix <- x$getinverse()
+        if (!is.null(ix)) {
+                message("getting cached inverse")
+                return(ix)
+        } 
+        
+        mat <- x$get()
+        ix <- solve(mat)
+        x$setinverse(ix)
+        ix
 }
